7cc66572980db1e401d557ffc4b92921202fc093
T. Michael Keesey <keesey@gmail.com>
2015-09-07 17:28:17 -0700

    Content-complete version.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,45 @@
 var gulp = require('gulp');
 
+var minifyHTML = require('gulp-minify-html');
 var rename = require("gulp-rename");
 var template = require('gulp-template');
 var webserver = require('gulp-webserver');
 
-var cvData = require('./data/cv.json');
- 
+var DATA = {
+	cv: require('./data/cv.json')
+};
+
+function handleError(error) {
+	console.error('' + error);
+	this.emit('end');
+}
+
 gulp.task('build', function ()
 {
     return gulp.src('templates/cv.html')
-        .pipe(template(cvData))
+        .pipe(template(DATA.cv))
+		.on('error', handleError)
+		.pipe(minifyHTML())
+		.on('error', handleError)
 		.pipe(rename('index.html'))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('serve', function() {
-	gulp.src('dist')
-		.pipe(webserver({
+gulp.task('serve', ['build', 'watch'], function() 
+{
+	return gulp.src('dist')
+		.pipe(webserver(
+		{
 			livereload: true,
 			directoryListing: false,
+			root: 'dist',
 			open: true
 		}));
 });
+
+gulp.task('watch', function()
+{
+	return gulp.watch(['data/*.json', 'templates/*.html'], ['build']);
+});
+
+gulp.task('default', ['serve']);
